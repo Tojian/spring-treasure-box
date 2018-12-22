2bbdc64650d55db2ef50349a3bc3557db1f18752
taojian <1099593012@qq.com>
2018-12-22 22:04:12 +0800

    mybatis todo

---
+++ tao-springboot-mybatis/src/main/java/com/taojian/mybatis/TaoSpringbootMybatisApplication.java
@@ -0,0 +1,14 @@
+package com.taojian.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaoSpringbootMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaoSpringbootMybatisApplication.class, args);
+    }
+
+}
+
