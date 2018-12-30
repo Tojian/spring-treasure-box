3b8d210c4985829df74b6b75d3a3985ba789a78d
taojian <1099593012@qq.com>
2018-12-30 13:27:59 +0800

    mybatis todo

---
+++ tao-springboot-mybatis/src/main/java/com/taojian/mybatis/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.taojian.mybatis.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: taojian
+ * @create: 2018-12-29 20:50
+ **/
+@RestController
+public class UserController {
+
+    @RequestMapping()
+    public String test(){
+
+        return null;
+    }
+}
---
+++ tao-springboot-mybatis/src/main/java/com/taojian/mybatis/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.taojian.mybatis.mapper;
+
+import com.taojian.mybatis.bean.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer userId);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer userId);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
